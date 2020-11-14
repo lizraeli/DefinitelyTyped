8508c58af85db1dbe8159506b511e2aba55e40ef
lizraeli <leo2002b@yahoo.com>
2020-11-14 20:14:22 +0900

    update persister

--- types/pollyjs__persister/index.d.ts
+++ types/pollyjs__persister/index.d.ts
@@ -1,13 +1,35 @@
-// Type definitions for @pollyjs/persister 4.3
+// Type definitions for @pollyjs/persister 5.0
 // Project: https://github.com/netflix/pollyjs/tree/master/packages/@pollyjs/persister
 // Definitions by: feinoujc <https://github.com/feinoujc>
 //                 silverchen <https://github.com/silverchen>
 //                 Offir Golan <https://github.com/offirgolan>
+//                 Lev Izraelit <https://github/lizraeli>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export default class Persister {
-    static readonly id: string;
+import { Polly, Request } from '@pollyjs/core';
+
+export interface Recording {
+    [key: string]: any;
+}
+
+export default class Persister<Options = { [key: string]: any }> {
+    static get id(): string;
     static readonly type: string;
-    readonly options: { [key: string]: any };
+    constructor(polly: Polly);
+    private _cache: Map<string, Recording>;
+    get options(): Options;
+    get defaultOptions(): {};
+    get hasPending(): boolean;
+    polly: Polly;
+    recordings: Map<string, Recording>;
     persist(): Promise<void>;
+    recordRequest(request: Request): void;
+    find(recordingId: string): Recording;
+    save(recordingId: string): void;
+    delete(recordingId: string): void;
+    findEntry(request: Request): void;
+    stringify(data: Recording): string;
+    findRecording(recordingId: string): Recording | null;
+    saveRecording(recordingId: string, data: Recording): void;
+    deleteRecording(recordingId: string): void;
 }
--- types/pollyjs__persister/pollyjs__persister-tests.ts
+++ types/pollyjs__persister/pollyjs__persister-tests.ts
@@ -1,7 +1,45 @@
-import Persister from '@pollyjs/persister';
+import Persister, { Recording } from '@pollyjs/persister';
+import { Polly } from '@pollyjs/core';
 
 Persister.id;
 Persister.type;
 
-const persister = new Persister();
-persister.options;
+class BasicCustomPersister extends Persister {}
+
+const basicPersister = new BasicCustomPersister(new Polly('recording'));
+
+basicPersister.options;
+
+interface DB {
+    [key: string]: Recording;
+}
+
+interface PersisterOptions {
+    db: DB;
+    save: (db: DB) => void;
+}
+
+class AdvancedCustomPersister extends Persister<PersisterOptions> {
+    db: DB;
+
+    constructor(polly: Polly) {
+        super(polly);
+        this.db = this.options.db;
+    }
+
+    findRecording(recordingId: string) {
+        return this.db[recordingId] || null;
+    }
+
+    saveRecording(recordingId: string, data: Recording) {
+        this.db[recordingId] = data;
+        this.options.save(this.db);
+    }
+}
+
+const advancedCustomPersister = new AdvancedCustomPersister(new Polly('recording'));
+
+advancedCustomPersister.db;
+advancedCustomPersister.options;
+advancedCustomPersister.findRecording('123');
+advancedCustomPersister.saveRecording('123', {});
